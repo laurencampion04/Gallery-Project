2e59d2fa1c2c92b36fc88b00589956867f76f2cb
laurencampion04 <152101998+laurencampion04@users.noreply.github.com>
2024-04-21 23:43:11 -0400

    project2

---
+++ src/image-gallery.js
@@ -0,0 +1,17 @@
+import { LitElement, html, css, } from 'lit'; 
+
+class ImageGallery extends LitElement {
+    static get styles() {
+        return 'project-two'; 
+    }
+
+    constructor() {
+        super();
+        
+    }
+
+    
+}
+
+globalThis.customElements.define(ImageGallery.tag, ProjectOne); 
+
